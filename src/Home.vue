b720167a56870d471b1e4da99a06297276afb850
Johnson-huang <2678683520@qq.com>
2022-04-29 02:41:41 +0800

    路由修改

--- src/Home.vue
+++ src/Home.vue
@@ -1,24 +1,40 @@
 <template>
   <div class="home-box">
-    <div class="item-box" v-for="(item) in urls" :key="item.path" @click="handleClick(item)">{{ item.meta.title }}</div>
+    <div class="list-box">
+      <div class="item-box" v-for="(item) in urls" :key="item.path">
+        <span class="text" v-if="!item.children || !item.children.length" @click="handleClick(item)">{{ item.meta.title }}</span>
+        <div class="list-box" v-else>
+          <div class="item-box" v-for="(subItem) in item.children" :key="subItem.path">
+            <span class="text" @click="handleClick(item, subItem)">{{subItem.meta.title}}</span>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import {defineComponent, reactive} from 'vue'
 import {useRouter} from 'vue-router'
+import {allRoutes} from "@/routers";
 
 export default defineComponent({
   setup() {
     const router = useRouter()
     const state = reactive({
-      urls: router.getRoutes().filter(item => item.path !== '/')
+      urls: allRoutes.filter(item => item.path !== '/')
     })
 
-    function handleClick(item) {
-      router.push({
-        path: item.path
-      })
+    function handleClick(item, subItem) {
+      if (!subItem) {
+        router.push({
+          path: item.path
+        })
+      } else {
+        router.push({
+          path: `${item.path}/${subItem.path}`
+        })
+      }
     }
 
     return {
@@ -32,14 +48,18 @@ export default defineComponent({
 <style lang="less" scoped>
 .home-box {
   padding: 20px;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-start;
+
+  .list-box {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+  }
 
   .item-box {
     color: blue;
     text-decoration: underline;
+    cursor: pointer;
 
     &:not(:last-child) {
       margin-bottom: 5px;
--- src/routers/index.js
+++ src/routers/index.js
@@ -1,22 +1,24 @@
-import { createRouter, createWebHashHistory } from 'vue-router';
-import Home from '@/Home.vue'
-import EarthAndMoon from '@/EarthAndMoon.vue'
-import Car from '@/Car.vue'
+import {createRouter, createWebHashHistory} from 'vue-router';
+import Autostereoscopy from "@/routers/autostereoscopy";
+
+export const allRoutes = [
+    {path: '/', component: () => import('@/Home.vue'), meta: {title: '首页'}},
+    {path: '/car', component: () => import('@/Car.vue'), meta: {title: '汽车模型'}},
+    {path: '/earth-and-moon', component: () => import('@/EarthAndMoon.vue'), meta: {title: '地月环绕'}},
+    // 裸眼3D
+    ...Autostereoscopy
+]
 
 // 创建路由实例并传递 `routes` 配置
 const router = createRouter({
-  // 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
-  history: createWebHashHistory(),
-  routes: [
-    { path: '/', component: Home, meta: {title: '首页'} },
-    { path: '/earth-and-moon', component: EarthAndMoon, meta: {title: '地月环绕'} },
-    { path: '/car', component: Car, meta: {title: '汽车模型'} },
-  ],
+    // 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
+    history: createWebHashHistory(),
+    routes: allRoutes
 });
 
 router.beforeEach((to, from, next) => {
-  document.title = to.meta.title;
-  next();
+    document.title = to.meta.title;
+    next();
 });
 
 export default router
