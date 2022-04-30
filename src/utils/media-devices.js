3dcd48d4cd57054cc5e8d087a56b173908b0274b
Johnson-huang <2678683520@qq.com>
2022-04-30 12:02:03 +0800

    人脸识别

---
+++ src/utils/media-devices.js
@@ -0,0 +1,53 @@
+/**
+ * * 是否支持访问用户媒体设备
+ * ? 支持，返回对应的支持函数
+ * ? 不支持，返回 false
+ */
+export function checkGetUserMediaSupport() {
+    return navigator.mediaDevices.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.getUserMedia
+}
+
+// 获取支持的音视频设备
+export function checkMediaDevices() {
+    return new Promise((resolve, reject) => {
+        if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices()) {
+            reject({
+                code: 0,
+                msg: '不支持'
+            })
+        } else {
+            navigator.mediaDevices.enumerateDevices()
+                .then((deviceInfos) => {
+                    resolve({
+                        code: 1,
+                        data: deviceInfos,
+                        msg: ''
+                    })
+                })
+                .catch((err) => {
+                    reject({
+                        code: 0,
+                        data: err,
+                        msg: err.message
+                    })
+                });
+        }
+    })
+}
+
+// 访问用户媒体设备的兼容方法
+export function getUserMedia(constraints, success, error) {
+    if (navigator.mediaDevices.getUserMedia) {
+        // 最新的标准API
+        navigator.mediaDevices.getUserMedia(constraints).then(success).catch(error);
+    } else if (navigator.webkitGetUserMedia) {
+        // webkit 核心浏览器
+        navigator.webkitGetUserMedia(constraints, success, error)
+    } else if (navigator.mozGetUserMedia) {
+        // firefox 浏览器
+        navigator.mozGetUserMedia(constraints, success, error);
+    } else if (navigator.getUserMedia) {
+        // 旧版API
+        navigator.getUserMedia(constraints, success, error);
+    }
+}
--- src/views/autostereoscopy/Tracking.vue
+++ src/views/autostereoscopy/Tracking.vue
@@ -1,12 +1,13 @@
 <template>
   <div class="page-box">
-    <video id="myVideo" width="400" height="300" preload autoplay loop muted controls></video>
+    <video ref="videoRef" id="myVideo" width="400" height="300" preload autoplay loop muted controls></video>
   </div>
 </template>
 
 <script>
 import 'tracking'
-import {defineComponent, onMounted} from 'vue'
+import {defineComponent, onMounted, ref} from 'vue'
+import {checkGetUserMediaSupport, checkMediaDevices, getUserMedia} from '@/utils/media-devices'
 
 export default defineComponent({
   setup() {
@@ -16,7 +17,32 @@ export default defineComponent({
     // 人脸识别 对应 裸眼3D 原理
     // https://xiaozhuanlan.com/topic/0241985376
 
-    onMounted(() => {
+    const videoRef = ref(null)
+
+    onMounted(async () => {
+      // 是否支持访问用户媒体设备
+      if (checkGetUserMediaSupport()) {
+        // 获取支持的音视频设备
+        const devicesResult = await checkMediaDevices()
+        // TODO 判断一下视频输入设备
+        if (devicesResult.code !== 0) {
+          console.log('devicesResult data:', devicesResult.data)
+        } else {
+          console.log('checkMediaDevices 错误：', devicesResult)
+        }
+
+        // 访问用户媒体设备
+        // getUserMedia(
+        //     {audio: false, video: true},
+        //     (stream) => {
+        //       videoRef.value.srcObject = stream;
+        //     },
+        //     (error) => {
+        //       console.log(`访问用户媒体设备失败${error.name}, ${error.message}`);
+        //     }
+        // )
+      }
+
       // console.log(window.tracking, 123)
       // const colors = new window.tracking.ColorTracker(['magenta', 'cyan', 'yellow']);
       //
@@ -33,7 +59,9 @@ export default defineComponent({
       // window.tracking.track('#myVideo', colors);
     })
 
-    return {}
+    return {
+      videoRef
+    }
   },
 })
 </script>
