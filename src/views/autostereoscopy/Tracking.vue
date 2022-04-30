065a7c043f6492c63bbbc1261ed94aa51ac876f6
Johnson-huang <2678683520@qq.com>
2022-04-30 21:23:52 +0800

    人脸识别

--- src/views/autostereoscopy/Tracking.vue
+++ src/views/autostereoscopy/Tracking.vue
@@ -11,8 +11,14 @@
 <script>
 import 'tracking'
 import 'tracking/build/data/face.js'
-import {defineComponent, onMounted, onUnmounted, reactive, ref} from 'vue'
+import * as THREE from 'three'
+import {defineComponent, onMounted, onUnmounted, ref} from 'vue'
 import {checkGetUserMediaSupport, checkMediaDevices, getUserMedia} from '@/utils/media-devices'
+
+let trackerTask = null
+let nextRX = 0
+let nextRY = 0
+let scene, camera, renderer, cube;
 
 export default defineComponent({
   setup() {
@@ -21,9 +27,6 @@ export default defineComponent({
 
     // 人脸识别 对应 裸眼3D 原理
     // https://xiaozhuanlan.com/topic/0241985376
-    const state = reactive({
-      trackerTask: null
-    })
 
     const videoRef = ref(null)
     const canvasRef = ref(null)
@@ -60,7 +63,7 @@ export default defineComponent({
             tracker.setInitialScale(4);
             tracker.setStepSize(2);
             tracker.setEdgesDensity(0.1);
-            state.trackerTask = tracking.track('#myVideo', tracker, {camera: true})
+            trackerTask = tracking.track('#myVideo', tracker, {camera: true})
 
             tracker.on('track', (event) => {
               // 清除之前的图形
@@ -72,6 +75,9 @@ export default defineComponent({
                 context.fillStyle = "#fff";
                 context.fillText(`x：${rect.x}px`, rect.x + rect.width + 5, rect.y + 11)
                 context.fillText(`y：${rect.y}px`, rect.x + rect.width + 5, rect.y + 22)
+
+                nextRY = 0.7 + rect.x / 320;
+                nextRX = 0.4 - rect.y / 240;
               })
             })
           },
@@ -81,15 +87,75 @@ export default defineComponent({
       )
     }
 
+    // 初始化 three
+    function init() {
+      scene = new THREE.Scene();
+
+      camera = new THREE.PerspectiveCamera(45, window.innerWidth / 2 / window.innerHeight, 0.1, 100);
+      camera.position.set(0, 0, 3);
+
+      renderer = new THREE.WebGLRenderer({
+        antialias: true,
+        alpha: true
+      });
+      renderer.setSize(window.innerWidth / 2, window.innerHeight);
+      document.querySelector(".box").appendChild(renderer.domElement);
+
+      // var controls = new THREE.OrbitControls(camera, renderer.domElement);
+
+      const geometry = new THREE.BoxGeometry(.6, .6, .6);
+      const material = new THREE.MeshStandardMaterial({color: 0x6698CB});
+      material.metalness = .44;
+      material.roughness = 0.4;
+      cube = new THREE.Mesh(geometry, material);
+      cube.rotation.set(.4, .7, 0);
+      scene.add(cube);
+
+      addLight();
+
+      loop();
+    }
+
+    function addLight() {
+      const ambientLight = new THREE.AmbientLight(0xd5d5d5);
+      ambientLight.intensity = 1.1;
+      scene.add(ambientLight);
+
+      const bottomRightDirLight = new THREE.DirectionalLight();
+      bottomRightDirLight.position.x = 5;
+      bottomRightDirLight.position.y = 3;
+      bottomRightDirLight.position.z = -5;
+      bottomRightDirLight.intensity = 1.3;
+      scene.add(bottomRightDirLight);
+
+      const frontDirLight = new THREE.DirectionalLight(0xffffff);
+      frontDirLight.position.x = -5;
+      frontDirLight.position.y = 5;
+      frontDirLight.position.z = 5;
+      frontDirLight.intensity = 1.3;
+      scene.add(frontDirLight);
+    }
+
+    function loop() {
+      requestAnimationFrame(loop);
+
+      cube.rotation.x += (nextRX - cube.rotation.x) / 5;
+      cube.rotation.y += (nextRY - cube.rotation.y) / 5;
+
+      renderer.render(scene, camera);
+    }
+
     onMounted(async () => {
       // 打开视频
       await openMedia()
+      // 初始化 three
+      init()
     })
 
     onUnmounted(() => {
-      if (state.trackerTask) {
+      if (trackerTask) {
         // 停止跟踪
-        state.trackerTask.stop()
+        trackerTask.stop()
       }
     })
 
@@ -102,36 +168,36 @@ export default defineComponent({
 </script>
 
 <style lang="less" scoped>
-  .page-box {
+.page-box {
+  display: flex;
+  justify-content: flex-start;
+  align-items: stretch;
+  height: 100vh;
+
+  .video-box {
+    width: 50%;
     display: flex;
-    justify-content: flex-start;
+    justify-content: stretch;
     align-items: stretch;
-    height: 100vh;
+    position: relative;
 
-    .video-box {
-      width: 50%;
-      display: flex;
-      justify-content: stretch;
-      align-items: stretch;
-      position: relative;
-
-      #myVideo {
-        width: 100%;
-      }
-
-      .canvas {
-        position: absolute;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        top: 0;
-        width: 100%;
-        height: 100%;
-      }
+    #myVideo {
+      width: 100%;
     }
 
-    .box {
-      width: 50%;
+    .canvas {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      top: 0;
+      width: 100%;
+      height: 100%;
     }
   }
+
+  .box {
+    width: 50%;
+  }
+}
 </style>
